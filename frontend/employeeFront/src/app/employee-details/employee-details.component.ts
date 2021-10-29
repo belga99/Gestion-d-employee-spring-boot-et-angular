53f6a70a96db4df3cff5abbd71d9aaa4279d8c44
tlig belgacem <tligbigo@gmail.com>
2021-10-30 01:21:25 +0200

    ajout le code de employee-details.component.ts

--- frontend/employeeFront/src/app/employee-details/employee-details.component.ts
+++ frontend/employeeFront/src/app/employee-details/employee-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Employee } from '../employee.model';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number
+  employee: Employee
+  constructor(private route: ActivatedRoute, private employeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeService.getEmployeeById(this.id).subscribe( data => {
+      this.employee = data;
+    });
   }
 
 }

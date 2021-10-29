0a2a4b66a1179665e9cd4b8df86cd4bada632cdd
tlig belgacem <tligbigo@gmail.com>
2021-10-30 01:09:03 +0200

    import HttpClientModule,FormsModule in app.module.ts

--- frontend/employeeFront/src/app/app.module.ts
+++ frontend/employeeFront/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
@@ -18,7 +19,9 @@ import { UpdateEmployeeComponent } from './update-employee/update-employee.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
